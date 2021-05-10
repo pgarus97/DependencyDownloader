bbf87fa3f40545225f53a72d817f81ea73d9959e
Philip Garus <philip.garus@capgemini.com>
2021-05-10 14:36:32 +0200

    replaced dir deletion method, updated readme, cleaned code

--- DownloaderProto/src/main/java/DependencyDownloader/DownloaderProto/DependencyDownloader.java
+++ DownloaderProto/src/main/java/DependencyDownloader/DownloaderProto/DependencyDownloader.java
@@ -7,6 +7,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.apache.commons.io.FileUtils;
+
 
 public class DependencyDownloader {
 
@@ -18,18 +20,24 @@ public class DependencyDownloader {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		//download dependencies from maven with source and src classifier
 		if(Files.exists(inputDir.resolve("pom.xml"))) {	
 			ProcessBuilder mvnInit = new ProcessBuilder("cmd","/c",  " mvn -Dclassifier=sources -DexcludeTransitive=true dependency:copy-dependencies");
-				mvnInit.directory(inputDir.toFile());
-				try {
-					Process p = mvnInit.start();
-					p.waitFor();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+			mvnInit.directory(inputDir.toFile());
+			ProcessBuilder mvnSRCInit = new ProcessBuilder("cmd","/c",  " mvn -Dclassifier=src -DexcludeTransitive=true dependency:copy-dependencies");
+			mvnSRCInit.directory(inputDir.toFile());
+			try {
+				Process p = mvnInit.start();
+				p.waitFor();
+				Process p2 = mvnSRCInit.start();
+				p2.waitFor();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+		}
 		
+		//download dependencies from npm 
 		if(Files.exists(inputDir.resolve("package.json"))) {	
 			ProcessBuilder npmInit = new ProcessBuilder("cmd","/c",  " npm install --legacy-peer-deps");
 				npmInit.directory(inputDir.toFile());
@@ -42,39 +50,34 @@ public class DependencyDownloader {
 				}
 			}
 		UnZipper zip = new UnZipper();
-    	String path = "Input/target/dependency";
-    	zip.unzipAll(path);
-    	Path depFolder = Paths.get("Input/target/dependency");
-        File index = depFolder.toFile();
-        String[]entries = index.list();
-        for(String s: entries){
-            File currentFile = new File(index.getPath(),s);
-            currentFile.delete();
-        }
+    	String sourcePath = "Input/target/dependency";
     	
+    	zip.unzipAll(sourcePath);
+    	
+    	//deletes output folder (sources.jars)
+    	File sourceOutDir = new File(sourcePath);
+		FileUtils.deleteDirectory(sourceOutDir);
+		
+        //move file directories into correct positions for scancode scan
     	Files.move(Paths.get("Input/node_modules"), Paths.get("Output/node_modules"), StandardCopyOption.REPLACE_EXISTING);
     	Files.move(Paths.get("Output"), Paths.get("scancode-toolkit-21.3.31/Output"), StandardCopyOption.REPLACE_EXISTING);
 
-    	
-    	ProcessBuilder npmInit = new ProcessBuilder("cmd","/c", "start " , " scancode -c -n 4 --json-pp output.json Output");
+    	//runs scancode scan over all downloaded sources
+    	ProcessBuilder scancodeInit = new ProcessBuilder("cmd","/c", "start " , " scancode -c -n 16 --json-pp output.json Output");
 		Path scancodeDir = Paths.get("scancode-toolkit-21.3.31");
-		npmInit.directory(scancodeDir.toFile());
+		scancodeInit.directory(scancodeDir.toFile());
 		try {
-			Process p = npmInit.start();
+			Process p = scancodeInit.start();
 			p.waitFor();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//only for testing 
-        Path unzipPath = Paths.get("scancode-toolkit-21.3.31/Output");
-        File index2 = unzipPath.toFile();
-        String[]entries2 = index2.list();
-        for(String s: entries2){
-            File currentFile = new File(index2.getPath(),s);
-            currentFile.delete();
-        }
+		//only for testing; deletes the created unzipped .jars and the node_modules 
+		File scanOutDir = new File("scancode-toolkit-21.3.31/Output");
+		FileUtils.deleteDirectory(scanOutDir);
 	}
+	
 }
 
 
